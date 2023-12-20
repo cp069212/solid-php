887ea33985b073cc68789664b99635886279a721
cp069212 <Clement_Papillon@etu.u-bourgogne.fr>
2023-12-20 11:18:47 +0100

    S as Single Responsibility Principle

---
+++ Code/exo-solid-1/Uploader.php
@@ -0,0 +1,10 @@
+<?php
+
+class Uploader
+{
+    public function getExtension()
+    {
+        $fileInformation = new FileInformation();
+        return $fileInformation->getExtension($this->name);
+    }
+}
